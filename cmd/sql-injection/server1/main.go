e889d2d66b497b1aed43b36bca22affc16773287
root <dposadallano@gmail.com>
2024-11-03 03:03:40 -0500

    sql injection using CLI

--- cmd/sql-injection/server1/main.go
+++ cmd/sql-injection/server1/main.go
@@ -1,1 +1,74 @@
 package main
+
+// Based on Mario Carrion Post:
+// https://mariocarrion.com/2021/10/22/golang-software-architecture-security-databases-sql-injection-permissions.html
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/url"
+
+	"github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib" // To initialize `pgx` DB driver
+)
+
+// Passing in an `id` like:
+//
+// "33ca99ce-f1d1-46c2-b26e-a0ff45011b18' OR ''='"
+//
+// The value "33ca..." is irrelevant because the `OR` is used instead.
+
+// go run main.go -id="33ca99ce-f1d1-46c2-b26e-a0ff45011b18' OR ''='"
+
+func main() {
+	var id string
+
+	flag.StringVar(&id, "id", "", "User ID to delete")
+
+	flag.Parse()
+
+	if id == "" {
+		fmt.Println("id is blank, exiting")
+		return
+	}
+
+	conn, err := newConn()
+	if err != nil {
+		log.Fatalln("Couldn't connect to DB", err)
+	}
+
+	//-
+
+	query := fmt.Sprintf("DELETE FROM users WHERE id = '%s'", id)
+
+	if _, err := conn.Exec(context.Background(), query); err != nil {
+		log.Fatalln("Couldn't delete", err)
+	}
+}
+
+func newConn() (*pgx.Conn, error) {
+	dsn := url.URL{
+		Scheme: "postgres",
+		User:   url.UserPassword("user", "password"),
+		Host:   "localhost:54329",
+		Path:   "dbname",
+	}
+
+	q := dsn.Query()
+	q.Add("sslmode", "disable")
+
+	dsn.RawQuery = q.Encode()
+
+	conn, err := pgx.Connect(context.Background(), dsn.String())
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	if err := conn.Ping(context.Background()); err != nil {
+		return nil, fmt.Errorf("db.Ping: %w", err)
+	}
+
+	return conn, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module falco-rules-examples
 
 go 1.21.6
+
+require github.com/jackc/pgx/v4 v4.18.3
+
+require (
+	github.com/jackc/chunkreader/v2 v2.0.1 // indirect
+	github.com/jackc/pgconn v1.14.3 // indirect
+	github.com/jackc/pgio v1.0.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.3.3 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgtype v1.14.4 // indirect
+	golang.org/x/crypto v0.28.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+)
